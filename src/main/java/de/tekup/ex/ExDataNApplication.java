f3988e61b7009dc818f10e6c947a65712ec718bd
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-07 19:06:20 +0100

    exercice

---
+++ src/main/java/de/tekup/ex/ExDataNApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExDataNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExDataNApplication.class, args);
+	}
+
+}
